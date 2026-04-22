6965df593a6efa831852f794e61839d5410f5841
cst10sites <mrkam.archive@gmail.com>
2026-04-21 23:47:50 -0700

    Add files via upload

---
+++ ethan/final/shots.js
@@ -0,0 +1,72 @@
+ /* function checkShot() {
+	var chosenShot = document.getElementById("shotValue").value;
+	
+	document.getElementById("shotDisplay").innerHTML = chosenShot;
+	
+} */
+
+
+
+function Shots(thisShot) {
+	if (thisShot == "net") {
+		document.getElementById("shotDisplay").innerHTML = "A net shot is an offensive play that the shuttler executes in the forecourt, close to the net. The birdie sails as close to the top of the net as possible and lands in the opponent's forecourt, usually very close to the net as well." ;
+		
+	}
+	if (thisShot == "lift") {
+		document.getElementById("shotDisplay").innerHTML = "The lift shot is a defensive shot in badminton. It is played when the shuttlecock is near the floor. The aim is to lift or lob the shuttlecock over the net and make the shuttlecock fall as near to the baseline as possible without hitting it out. It can be played both with the forehand or backhand.";
+		
+	}
+	if (thisShot == "drive") {
+		document.getElementById("shotDisplay").innerHTML = "The drive is played from mid-court to mid-court and is a quick, flat, powerful counter-attacking shot. It can be both a backhanded and forehanded shot. When it is executed correctly, a player will force their opponent to hit an upward return.";
+		
+	}
+	if (thisShot == "smash") {
+		document.getElementById("shotDisplay").innerHTML = "The badminton smash is mainly used for attacking. It often acts as a winning shot in any badminton rallies. Basically a smash is an offensive shot shot fired from a high point and travels down steeply towards your opponent. Types of Smash. There are few types of smashes that are commonly used in badminton games today.";
+		
+	}
+	if (thisShot == "drop") {
+		document.getElementById("shotDisplay").innerHTML = "It is a soft badminton shot performed from the Rear Court. It travels down steeply and land at your opponent's forecourt.";
+		
+	}
+	if (thisShot == "clear") {
+		document.getElementById("shotDisplay").innerHTML = "The clear shot is one of the most common strokes used in badminton to hit the birdie high and deep, preferably all the way to the end line. This should definitely be used if you need to earn some time to return to the center of the court by driving your opponent to the rear end of the court.";
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
